7e47d64561646049b71211b4f2ec9309ff4c2c9b
Louis Gérard <louis.gerard@isen.yncrea.fr>
2017-02-03 19:23:00 -0500

    Première implémentation serveur

--- src/ca/uqac/poo/devoir1/ApplicationServeur.java
+++ src/ca/uqac/poo/devoir1/ApplicationServeur.java
@@ -1,14 +1,28 @@
 package ca.uqac.poo.devoir1;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.net.*;
+import java.time.LocalDateTime;
 
 /**
  * Created by dhawo on 03/02/2017.
  */
 public class ApplicationServeur {
+    private ServerSocket welcomeSocket = null;
+    private String sourceFolder;
+    private String classFolder;
+    private String outputFile;
+    private Socket connectionSocket;
+
     /**
      * prend le numéro de port, crée un SocketServer sur le port
      */
     public ApplicationServeur (int port){
-
+        try{
+            welcomeSocket = new ServerSocket(port);
+        }catch(IOException ex){
+            log("Le Socket n'as pas pu être bindé sur le port donné.")
+        }
     }
 
     /**
@@ -17,7 +31,19 @@ public class ApplicationServeur {
      * le client, et appellera traiterCommande(Commande uneCommande)
      */
     public void aVosOrdres() {
-
+        while(true){
+            try{
+                connectionSocket = welcomeSocket.accept(); //Accepte la connexion (une seule à la fois dans ce cas)
+                ObjectInputStream inputFromClient = new ObjectInputStream(connectionSocket.getInputStream()); //Créer le Stream d'entrée
+                Commande commande = (Commande)inputFromClient.readObject(); //Récupération de la commande
+                traiteCommande(commande); //Traitement de la commande
+                connectionSocket.close();
+            }catch(IOException ex){
+                System.out.println("La connexion n'as pas pu etre acceptée");
+            }catch(ClassNotFoundException ex){
+                System.out.println("Le message recu n'est pas une commande");
+            }
+        }
     }
 
     /**
@@ -33,7 +59,19 @@ public class ApplicationServeur {
      * socket
      */
     public void traiterLecture(Object pointeurObjet, String attribut) {
-
+        Class c = pointeurObjet.getClass();
+        try{
+            Field field = c.getField(attribut);
+            Object value = field.get(pointeurObjet);
+            ObjectOutputStream outputToClient = new ObjectOutputStream(connectionSocket.getOutputStream()); //Création du Stream de sortie
+            outputToClient.writeObject(value);
+        }catch(NoSuchFieldException ex){
+            log(ex.getMessage());
+        }catch(IllegalAccessException ex){
+            log(ex.getMessage());
+        }catch(IOException ex){
+            log(ex.getMessage());
+        }
     }
 
     /**
@@ -75,17 +113,45 @@ public class ApplicationServeur {
      * types des arguments, et un tableau d’arguments pour la fonction. Le résultat de la
      * fonction est renvoyé par le serveur au client (ou le message que tout s’est bien
      * passé)
-     /**
-     public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types,
-     Object[] valeurs) {…}
+     */
+    public void traiterAppel(Object pointeurObjet, String nomFonction, String[] types, Object[] valeurs){
 
-     /**
+    }
+
+    /**
      * programme principal. Prend 4 arguments: 1) numéro de port, 2) répertoire source, 3)
      * répertoire classes, et 4) nom du fichier de traces (sortie)
      * Cette méthode doit créer une instance de la classe ApplicationServeur, l’initialiser
      * puis appeler aVosOrdres sur cet objet
      */
     public static void main(String[] args) {
+        if(args.length != 4){
+            System.out.println("Mauvais arguments");
+            System.out.println("Utilisation : ");
+            System.out.println("1) numéro de port, 2) répertoire source, 3) répertoire classes, et 4) nom du fichier de traces (sortie)");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        ApplicationServeur serveur = new ApplicationServeur(port);
+        serveur.sourceFolder = args[1];
+        serveur.classFolder = args[2];
+        serveur.outputFile = args[3];
+        if(serveur.welcomeSocket != null){
+            serveur.aVosOrdres();
+        }
+    }
 
+    private void log(String message){
+        try
+        {
+            FileWriter fw = new FileWriter(outputFile);
+            fw.write (LocalDateTime.now().toString() + message);
+            fw.write ("\r\n");
+            fw.close();
+        }
+        catch (IOException exception)
+        {
+            System.out.println ("Erreur lors de l'écriture : " + exception.getMessage());
+        }
     }
 }
