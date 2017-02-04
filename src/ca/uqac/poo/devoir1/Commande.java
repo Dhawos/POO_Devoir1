2e5fdc3c6d479eaf5a89e519a571486fc44affd6
Edouard <edouard.samyn@gmail.com>
2017-02-03 19:17:39 -0500

    Command implementation

--- src/ca/uqac/poo/devoir1/Commande.java
+++ src/ca/uqac/poo/devoir1/Commande.java
@@ -1,10 +1,25 @@
 package ca.uqac.poo.devoir1;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by dhawo on 03/02/2017.
  */
 public class Commande implements Serializable {
+    private String type;
+    private ArrayList<String> arguments;
 
+    public Commande(String type, ArrayList<String> arguments){
+        this.arguments = arguments;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
